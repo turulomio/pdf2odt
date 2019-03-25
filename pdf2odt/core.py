c60496282f386824879158b6e54575430a5e1506
turulomio <turulomio@yahoo.es>
2019-03-25 23:32:05 +0100

    Now works

--- pdf2odt/core.py
+++ pdf2odt/core.py
@@ -3,6 +3,7 @@
 import datetime
 import platform
 import sys
+import glob
 
 if platform.system()=="Windows":
     print("This script only works on Linux")
@@ -16,6 +17,9 @@ import pkg_resources
 
 from colorama import Fore, Style, init as colorama_init
 from pdf2odt.version import __versiondate__, __version__
+from officegenerator import ODT_Standard
+from odf.text import P
+from PIL import Image
 
 try:
     t=gettext.translation('pdf2odt',pkg_resources.resource_filename("pdf2odt","locale"))
@@ -29,11 +33,11 @@ except:
 ## @param arguments is an array with parser arguments. For example: ['--max_files_to_store','9']. 
 def main(arguments=None):
     start=datetime.datetime.now()
-    parser=argparse.ArgumentParser(prog='pdf2odt', description=_('Converts a pdf to a LibreOffice Writer document with pages as images'), epilog=_("Developed by Mariano Muñoz 2018-{}".format(__versiondate__.year)), formatter_class=argparse.RawTextHelpFormatter)
+    parser=argparse.ArgumentParser(prog='pdf2odt', description=_('Converts a pdf to a LibreOffice Writer document with pages as images'), epilog=_("Developed by Mariano Muñoz 2019-{}".format(__versiondate__.year)), formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('--version', action='version', version=__version__)
 
-    parser.add_argument('--pdf', help=_("File owner will be changed to this parameter. It does nothing if it's not set."), action="store", default=None)
-    parser.add_argument('absolute_path', help=_("Directory who is going to be changed permissions and owner recursivily"), action="store")
+    parser.add_argument('--pdf', help=_("PDF file to convert"), action="store", default=None)
+    parser.add_argument('output', help=_("Output odt file"), action="store")
 
     args=parser.parse_args(arguments)
 
@@ -44,3 +48,25 @@ def main(arguments=None):
         locale.setlocale(locale.LC_ALL, ".".join(locale.getlocale()))
     except:
         pass
+
+
+    os.system("pdftoppm -png '{}' 'pdf2odt_temporal'".format(args.pdf))
+
+    doc=ODT_Standard(args.output)
+    doc.setMetadata("OfficeGenerator title", "OfficeGenerator subject", "Turulomio")
+
+    for filename in sorted(glob.glob("pdf2odt_temporal*.png")):
+        img = Image.open(filename)
+        x,y=img.size
+        cmx=17
+        cmy=y*cmx/x
+        doc.addImage(filename, filename)
+        p = P(stylename="Illustration")
+        p.addElement(doc.image(filename, cmx,cmy))
+        doc.insertInCursor(p, after=True)
+        p = P(stylename="Standard")
+        doc.insertInCursor(p, after=True)
+    doc.save()
+
+    for filename in glob.glob("pdf2odt_temporal*.png"):
+        os.system("rm '{}'".format(filename))
