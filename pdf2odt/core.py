8a0308051d566ce993399fd23d2e36986f55c66b
turulomio <turulomio@yahoo.es>
2021-03-24 12:46:36 +0100

    Fixed bug with tesseract parameter position. Thanks @maxlem-neuralium. Now temporal files are generated with tempfile  module.

--- pdf2odt/core.py
+++ pdf2odt/core.py
@@ -1,25 +1,28 @@
 ## @namespace pdf2odt.core
 ## @brief Core functions of the package
-import datetime
-import platform
-import glob
-import argparse
-import gettext
-import os
-import pkg_resources
 
+from argparse import ArgumentParser, RawTextHelpFormatter
 from colorama import Fore, Style, init as colorama_init
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from datetime import datetime
+from gettext import translation
+from glob import glob
+from multiprocessing import cpu_count
+from PIL import Image
 from pdf2odt.version import __versiondate__, __version__
+from pkg_resources import resource_filename
+from platform import system as platform_system
 from officegenerator import ODT_Standard
 from odf.text import P
-from PIL import Image
-from multiprocessing import cpu_count
+from os import chdir,  path,  getcwd
+from shutil import copyfile
 from subprocess import check_output, STDOUT
+from tempfile import TemporaryDirectory
 from tqdm import tqdm
 from sys import exit
 
 try:
-    t=gettext.translation('pdf2odt',pkg_resources.resource_filename("pdf2odt","locale"))
+    t=translation('pdf2odt', resource_filename("pdf2odt","locale"))
     _=t.gettext
 except:
     _=str
@@ -31,7 +34,7 @@ def poppler_check_is_pdf(filename):
     return True
 
 def poppler_get_pdf_num_pages(filename):
-    if platform.system()=="Windows":
+    if platform_system()=="Windows":
         pdfinfo_command='pdfinfo.exe "{}"'.format( filename) #I add quotes to embrace all command too
     else:
         pdfinfo_command="pdfinfo '{}'".format(filename)
@@ -47,7 +50,7 @@ def poppler_get_pdf_num_pages(filename):
 ## Returns a list of tesseract supported languages
 ## @return list of strings with supported languages
 def tesseract_get_supported_languages():
-    if platform.system()=="Windows":
+    if platform_system()=="Windows":
         command='tesseract.exe --list-langs'
     else:
         command='tesseract --list-langs'
@@ -64,16 +67,16 @@ def tesseract_get_supported_languages():
         pass
     return result
     
-def process_page(args, number, numpages):
-    zfill=str(number).zfill(len(str(numpages)))
-    pngfile="pdf2odt_temporal-{}.png".format(zfill)
-    
-    if platform.system()=="Windows":
-        pdftoppm_command='pdftoppm.exe -r {0} -f {1} -l {1} -png "{2}" pdf2odt_temporal'.format( args.resolution,  number,  args.pdf) #I add quotes to embrace all command too
-        tesseract_command='tesseract.exe {0} -l {1} {0}'.format(pngfile, args.tesseract_language)#I add quotes to embrace all command too
+def process_pdf_page(args, number, numpages):
+    zfill=str(number).zfill(len(str(numpages))) 
+    if platform_system()=="Windows":
+        pdftoppm_command=f"pdftoppm.exe -r {args.resolution} -f {number} -l {number} -png file.pdf pdfpage"
+        tesseract_command=f"tesseract.exe pdfpage-{zfill}.png pdfpage-{zfill} -l {args.tesseract_language}"
     else:
-        pdftoppm_command="pdftoppm -png -r {0} -f {1} -l {1} '{2}' pdf2odt_temporal".format( args.resolution, number, args.pdf)
-        tesseract_command="tesseract {0} -l {1} {0}".format(pngfile, args.tesseract_language)
+        pdftoppm_command=f"pdftoppm -r {args.resolution} -f {number} -l {number} -png file.pdf pdfpage"
+        tesseract_command=f"tesseract pdfpage-{zfill}.png pdfpage-{zfill} -l {args.tesseract_language}"
+    #print(pdftoppm_command)
+    #print(tesseract_command)
     check_output(pdftoppm_command, shell=True,  stderr=STDOUT)
     if args.tesseract==True:
         check_output(tesseract_command, shell=True,  stderr=STDOUT)
@@ -84,8 +87,8 @@ def process_page(args, number, numpages):
 ## You can call with main(['--pretend']). It's equivalento to os.system('pdf2odt --pretend')
 ## @param arguments is an array with parser arguments. 
 def main(arguments=None):
-    start=datetime.datetime.now()
-    parser=argparse.ArgumentParser(prog='pdf2odt', description=_('Converts a pdf to a LibreOffice Writer document with pages as images'), epilog=_("Developed by Mariano Muñoz 2019-{}".format(__versiondate__.year)), formatter_class=argparse.RawTextHelpFormatter)
+    start=datetime.now()
+    parser=ArgumentParser(prog='pdf2odt', description=_('Converts a pdf to a LibreOffice Writer document with pages as images'), epilog=_("Developed by Mariano Muñoz 2019-{}".format(__versiondate__.year)), formatter_class=RawTextHelpFormatter)
     parser.add_argument('--version', action='version', version=__version__)
     parser.add_argument('--pdf', help=_("PDF file to convert"), action="store", default=None, required=True)
     parser.add_argument('--tesseract_language', help=_("Language used in tesseract command. Default is eng"), action="store", default="eng")
@@ -94,6 +97,8 @@ def main(arguments=None):
     parser.add_argument('output', help=_("Output odt file"), action="store")
 
     args=parser.parse_args(arguments)
+    
+    cwd=getcwd()
 
     colorama_init(autoreset=True)
     
@@ -114,43 +119,48 @@ def main(arguments=None):
         else:
             print(Style.BRIGHT +_("Using '{}' language for Tesseract OCR.").format(Fore.GREEN + args.tesseract_language + Fore.WHITE))
 
-    #Launching concurrent process
-    futures=[]
-    from concurrent.futures import ProcessPoolExecutor, as_completed
-    executor = ProcessPoolExecutor(max_workers=cpu_count()+1)
-    for number in range(numpages):
-        futures.append(executor.submit(process_page, args, number+1, numpages))
-    for f in tqdm(as_completed(futures), total=len(futures)):
-        pass
 
-    #Generating ODT
-    doc=ODT_Standard(args.output)
-    pdf=os.path.basename(args.pdf)
-    odt=os.path.basename(args.output)
-    doc.setMetadata(_("Converting PDF to ODT"),  _("Converting {} to {} using odt2pdf-{}").format(pdf, odt, __version__), "odt2pdf")
-    
-    for filename in sorted(glob.glob("pdf2odt_temporal*.png")):
-        img = Image.open(filename)
-        x,y=img.size
-        cmx=17
-        cmy=y*cmx/x
-        img.close()
-        doc.addImage(filename, filename)
-        p = P(stylename="Illustration")
-        p.addElement(doc.image(filename, cmx,cmy))
-        doc.insertInCursor(p, after=True)
-        if args.tesseract==True:
-            for line in open(filename +".txt", "r", encoding='UTF-8').readlines():
-                p=P(stylename="Standard")
-                p.addText(line)
-                doc.insertInCursor(p, after=True)
+    with TemporaryDirectory() as tmpdirname:#Exiting this with tmpdirname is deleted. To debug you must do it inside this with
         
-    doc.save()
+        #Copy pdf to temporal dir
+        copyfile(args.pdf, f"{tmpdirname}/file.pdf")
+        chdir(tmpdirname)
+        
+        #Launching concurrent process
+        futures=[]
+        executor = ProcessPoolExecutor(max_workers=cpu_count())
+        for number in range(numpages):
+            futures.append(executor.submit(process_pdf_page, args, number+1, numpages))
+        for f in tqdm(as_completed(futures), total=len(futures)):
+            pass
 
-    #Removing temporal files
-    for filename in glob.glob("pdf2odt_temporal*.png"):
-        os.remove(filename)
-        if args.tesseract==True:
-            os.remove(filename+".txt")
+        #Generating ODT
+        doc=ODT_Standard("file.odt")
+        pdf=path.basename(args.pdf)
+        odt=path.basename(args.output)
+        doc.setMetadata(_("Converting PDF to ODT"),  _("Converting {} to {} using odt2pdf-{}").format(pdf, odt, __version__), "odt2pdf")
         
-    print(Style.BRIGHT + _("ODT generation took {}").format(Fore.GREEN + str(datetime.datetime.now()-start)))
+        for filename in sorted(glob("pdfpage*.png")):
+            img = Image.open(filename)
+            x,y=img.size
+            cmx=17
+            cmy=y*cmx/x
+            img.close()
+            doc.addImage(filename, filename)
+            p = P(stylename="Illustration")
+            p.addElement(doc.image(filename, cmx,cmy))
+            doc.insertInCursor(p, after=True)
+            if args.tesseract==True:
+                for line in open(filename[:-4] +".txt", "r", encoding='UTF-8').readlines():
+                    p=P(stylename="Standard")
+                    p.addText(line)
+                    doc.insertInCursor(p, after=True)
+        doc.save()
+        
+        # Copies generated file to output
+        chdir(cwd)
+        copyfile(f"{tmpdirname}/file.odt", args.output)
+   
+        #system(f"ls -la {tmpdirname}")
+        
+    print(Style.BRIGHT + _("ODT generation took {}").format(Fore.GREEN + str(datetime.now()-start)))
